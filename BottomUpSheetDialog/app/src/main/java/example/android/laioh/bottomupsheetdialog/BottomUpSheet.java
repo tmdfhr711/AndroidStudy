b14dee94ecb2b5c83f17796c131e700445975740
OHRok <tmdfhr711@gmail.com>
2016-07-29 01:29:34 +0900

    first commit

---
+++ BottomUpSheetDialog/app/src/main/java/example/android/laioh/bottomupsheetdialog/BottomUpSheet.java
@@ -0,0 +1,13 @@
+package example.android.laioh.bottomupsheetdialog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BottomUpSheet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_up_sheet);
+    }
+}
