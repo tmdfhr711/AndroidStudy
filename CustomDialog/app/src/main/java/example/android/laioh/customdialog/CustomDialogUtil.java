3bb32180009d06f514751a7f44c15541c2ac77b3
OHRok <tmdfhr711@gmail.com>
2016-11-02 13:05:23 +0900

    Create CustomDialog

---
+++ CustomDialog/app/src/main/java/example/android/laioh/customdialog/CustomDialogUtil.java
@@ -0,0 +1,15 @@
+package example.android.laioh.customdialog;
+
+import android.app.Dialog;
+import android.content.Context;
+
+/**
+ * Created by Lai.OH on 2016-09-09.
+ */
+public class CustomDialogUtil extends Dialog {
+
+
+    public CustomDialogUtil(Context context) {
+        super(context);
+    }
+}
