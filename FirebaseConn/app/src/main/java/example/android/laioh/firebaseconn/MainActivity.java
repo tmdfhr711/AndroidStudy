45e53f97f583fac96cc2b519dea68d214ac5a7ec
OHRok <tmdfhr711@gmail.com>
2017-02-14 20:41:50 +0900

    구글 계정 로그인 구현중...

--- FirebaseConn/app/src/main/java/example/android/laioh/firebaseconn/MainActivity.java
+++ FirebaseConn/app/src/main/java/example/android/laioh/firebaseconn/MainActivity.java
@@ -1,13 +1,31 @@
 package example.android.laioh.firebaseconn;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    Button go_signin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        go_signin = (Button) findViewById(R.id.go_signin);
+
+        go_signin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.go_signin:
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+        }
     }
 }
